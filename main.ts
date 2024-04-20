f7ad708e8f51f0e07c3d77bc4fb688d8b259cd90
mutahirshah11 <mutahirhps@gmail.com>
2024-04-20 22:48:48 +0500

    Todo app

---
+++ main.ts
@@ -0,0 +1,32 @@
+import inquirer from "inquirer" ; 
+
+let ToDo = [] ;
+let condition = true; 
+
+while (condition) { 
+let ToDoQuestions = await inquirer.prompt(
+[    
+        {
+            name : "firstQuestion", 
+            type : "input" ,
+            message : "what do you want to add to your ToDo's ?", 
+        },
+        {
+            name :'secondQuestion',
+            type : 'confirm',
+            message : 'Do you want to Add more ?',
+            default : "true"
+        }, 
+       
+        
+    ]
+) ;
+
+ToDo.push(ToDoQuestions.firstQuestion) ; 
+condition = ToDoQuestions.secondQuestion ;
+console.log(ToDo)  ;
+
+} 
+
+
+
